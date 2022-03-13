cb29fb98637ecf415caabbe6d6d398db9145310e
Alex Candido <acs2@ecomp.poli.br>
2022-03-12 23:37:59 -0300

    Dialyzer Fix

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule ElixirHttpServer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      elixirc_options: [debug_info: Mix.env() == :dev],
       deps: deps()
     ]
   end
@@ -22,8 +23,7 @@ defmodule ElixirHttpServer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
