50ab33326848cd88a2f0565b965f95ee8a0921a1
Alex Candido <acs2@ecomp.poli.br>
2022-03-13 09:31:12 -0300

    refactor: normal parameters instead of a tuple

--- lib/server.ex
+++ lib/server.ex
@@ -7,58 +7,43 @@ defmodule ElixirHttpServer.Server do
     packet: 0
   ]
 
-  @spec run(integer()) :: :error | :ok
   def run(port \\ 8000) do
     IO.puts(IO.ANSI.clear() <> IO.ANSI.home())
     IO.puts("[SERVER] iniciando servidor na porta #{port}")
-    handle({:listen, port})
+    handle_listen(port)
   end
 
-  @spec handle(
-          {:listen, integer()}
-          | {:accept, socket}
-          | {:close, conn_params | data_params}
-          | {:receive, socket}
-          | {:error, {error_code, error_message}}
-          | {:data, data_params}
-        ) :: :error | :ok
-        when error_message: String.t(),
-             error_code: atom(),
-             conn_params: %{client: socket},
-             data_params: %{client: socket, data: binary()}
-
-  def handle({:listen, port}) do
+  def handle_listen(port) do
     case :gen_tcp.listen(port, @options) do
-      {:ok, socket} -> handle({:accept, socket})
-      {:error, reason} -> handle({:error, {reason, "falha ao inicializar o servidor"}})
+      {:ok, socket} -> handle_accept(socket)
+      {:error, reason} -> handle_error(reason, "falha ao inicializar o servidor")
     end
   end
 
-  def handle({:accept, listening_socket}) do
+  def handle_accept(listening_socket) do
     IO.puts("aceitando conexão...")
 
     case :gen_tcp.accept(listening_socket) do
       {:ok, socket} ->
-        handle({:receive, %{client: socket}})
+        handle_receive(socket)
         # Reinicia recursivamente pra atender a proxima requisição
-        handle({:accept, listening_socket})
+        handle_accept(listening_socket)
 
       {:error, reason} ->
-        handle({:error, {reason, "falha ao aceita socket"}})
+        handle_error(reason, "falha ao aceita socket")
     end
   end
 
-  def handle({:receive, conn}) do
+  def handle_receive(client) do
     IO.puts("recebendo dados...")
 
-    case :gen_tcp.recv(conn.client, 0) do
-      {:ok, data} -> handle({:data, Map.put(conn, :data, data)})
-      {:error, reason} -> handle({:error, {reason, "falha ao receber dados"}})
+    case :gen_tcp.recv(client, 0) do
+      {:ok, data} -> handle_data(client, data)
+      {:error, reason} -> handle_error(reason, "falha ao receber dados")
     end
   end
 
-  def handle({:data, conn}) do
-    data = conn.data
+  def handle_data(client, data) do
     IO.puts("Dados Recebidos:")
     IO.puts(to_string(data) <> "\r\n")
 
@@ -71,22 +56,22 @@ defmodule ElixirHttpServer.Server do
         "\r\n\r\n" <>
         message
 
-    case :gen_tcp.send(conn.client, payload) do
-      :ok -> handle({:close, conn})
-      {:error, reason} -> handle({:error, {reason, "falha ao responder requisição"}})
+    case :gen_tcp.send(client, payload) do
+      :ok -> handle_close(client)
+      {:error, reason} -> handle_error(reason, "falha ao responder requisição")
     end
   end
 
-  def handle({:close, conn}) do
+  def handle_close(client) do
     try do
-      :ok = :gen_tcp.shutdown(conn.client, :read_write)
+      :ok = :gen_tcp.shutdown(client, :read_write)
       IO.puts("Conexão fechada!")
     rescue
-      x -> handle({:error, {x.message, "falha ao fechar conexão"}})
+      x -> handle_error(x.message, "falha ao fechar conexão")
     end
   end
 
-  def handle({:error, {reason, message}}) do
+  def handle_error(reason, message \\ "erro_inesperado") do
     message = IO.ANSI.format([:red_background, :black, "[SERVER] #{message}. ERRCODE: #{reason}"])
     IO.puts(:stderr, message)
     :error
